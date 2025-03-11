91344b7b9bbcaec42c74272dd68ede616491d19c
Mohammad Aakhil <149184615+Mraakhil@users.noreply.github.com>
2025-03-12 03:44:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 #1 -this will create a S3 bucket in AWS
 resource "aws_s3_bucket" "terraform_state_s3" {
   #make sure you give unique bucket name
-  bucket = "terraformmm-coachdevops-stateee" 
+  bucket = "terraformmm-coachdevops-stateeeEEEEEEE" 
   force_destroy = true
 # Enable versioning to see full revision history of our state files
   versioning {
@@ -37,7 +37,7 @@ resource "aws_dynamodb_table" "terraform_locks" {
 terraform {
   backend "s3" {
     #Replace this with your bucket name!
-    bucket         = "terraformmm-coachdevops-stateee"
+    bucket         = "terraformmm-coachdevops-stateeeEEEEEEE"
     key            = "dc/s3/terraform.tfstate"
     region         = "us-east-2"
     #Replace this with your DynamoDB table name!
