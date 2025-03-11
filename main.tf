9ff37afa18f83520fa6f5b31d48ccc779dfc9161
Mohammad Aakhil <149184615+Mraakhil@users.noreply.github.com>
2025-03-12 03:54:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,6 +28,8 @@ resource "aws_dynamodb_table" "terraform_locks" {
   name         = "tf-up-and-run-locksPPPPPPPP"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
+  use_lockfile = true
+
         attribute {
          name = "LockID"
          type = "S"
