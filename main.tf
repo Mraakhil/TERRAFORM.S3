623a0dd36aac9d23b7ee060b95cf793d3132bfdc
Mohammad Aakhil <149184615+Mraakhil@users.noreply.github.com>
2025-03-12 03:09:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,12 @@ provider "aws" {
   region     = var.region
 }
 
+
 #Step 3 - Creates S3 backend
 terraform {
   backend "s3" {
     #Replace this with your bucket name!
-    bucket         = "terraform-coachdevops-state"
+    bucket         = "terraform-coachdevops-stateeeeeeeeeeeeeee"
     key            = "dc/s3/terraform.tfstate"
     region         = "us-east-2"
     #Replace this with your DynamoDB table name!
