43654d4ca90092783240d4c364fa4975f3d8acbc
Mohammad Aakhil <149184615+Mraakhil@users.noreply.github.com>
2025-03-12 03:05:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,23 +2,16 @@ provider "aws" {
   region     = var.region
 }
 
-#1 -this will create a S3 bucket in AWS
-resource "aws_s3_bucket" "terraform_state_s3" {
-  #make sure you give unique bucket name
-  bucket = "terraform-coachdevops-stateeeeeeeeeeeeeee" 
-  force_destroy = true
-# Enable versioning to see full revision history of our state files
-  versioning {
-         enabled = true
-        }
-
-# Enable server-side encryption by default
-server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+#Step 3 - Creates S3 backend
+terraform {
+  backend "s3" {
+    #Replace this with your bucket name!
+    bucket         = "terraform-coachdevops-state"
+    key            = "dc/s3/terraform.tfstate"
+    region         = "us-east-2"
+    #Replace this with your DynamoDB table name!
+    dynamodb_table = "tf-up-and-run-locks"
+    encrypt        = true
     }
-  }
 }
 
